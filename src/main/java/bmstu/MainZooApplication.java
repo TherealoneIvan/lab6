974129633a2a781aef0962fee4ab530892b1bd95
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-23 00:11:44 +0300

    Scripted auto-commit on change (2020-12-23 00:11:44) by gitwatch.sh

--- src/main/java/bmstu/MainZooApplication.java
+++ src/main/java/bmstu/MainZooApplication.java
@@ -43,7 +43,7 @@ public class MainZooApplication  extends AllDirectives {
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> system.terminate());
     }
-    private CompletionStage<HttpRequest>
+    private CompletionStage<HttpRequest> single
     private Route createRoute(){
         return get(()->
                 parameter("url", url->
