8ad1f97d2094874dd2da952defc4205327ae1eb2
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-23 01:28:34 +0300

    Scripted auto-commit on change (2020-12-23 01:28:34) by gitwatch.sh

--- src/main/java/bmstu/MainZooApplication.java
+++ src/main/java/bmstu/MainZooApplication.java
@@ -49,7 +49,7 @@ public class MainZooApplication  extends AllDirectives {
     private static CompletionStage<HttpResponse> singleReq (String url){
         return http.singleRequest(HttpRequest.create(url));
     }
-    private static CompletionStage<HttpResponse> fetch (String url , String host, String sUrl , ){
+    private static CompletionStage<HttpResponse> fetch (String url , String host, String sUrl , String count){
         return  http.singleRequest(HttpRequest.create());
     }
     private Route createRoute(){
