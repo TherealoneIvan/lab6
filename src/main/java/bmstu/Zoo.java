56de828a1d514cf9c46b2509abad12fc172648b8
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-22 21:17:42 +0300

    Scripted auto-commit on change (2020-12-22 21:17:42) by gitwatch.sh

--- src/main/java/bmstu/Zoo.java
+++ src/main/java/bmstu/Zoo.java
@@ -10,13 +10,13 @@ public class Zoo {
     zoo.create("/servers/s",
     ZooDefs.Ids.OPEN_ACL_UNSAFE ,
     CreateMode.EPHEMERAL_SEQUENTIAL);
-    List<String> servers = zoo.getChildren("/servers", this);
-    for (String s : servers) {
-        byte[] data = zoo.getData("/servers/" + s, false, null);
-        System.out.println("server " + s + " data=" + new String(data));
-    }
-    private Watcher watcher (){
-        
+
+    private Watcher watcher () {
+        List<String> servers = zoo.getChildren("/servers", this);
+        for (String s : servers) {
+            byte[] data = zoo.getData("/servers/" + s, false, null);
+            System.out.println("server " + s + " data=" + new String(data));
+        }
     }
 
     public Zoo() throws IOException {
