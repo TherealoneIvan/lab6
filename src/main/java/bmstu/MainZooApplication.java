c408389db52210ab9d93e896c734983a7a0e07da
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-23 01:06:30 +0300

    Scripted auto-commit on change (2020-12-23 01:06:30) by gitwatch.sh

--- src/main/java/bmstu/MainZooApplication.java
+++ src/main/java/bmstu/MainZooApplication.java
@@ -56,7 +56,7 @@ public class MainZooApplication  extends AllDirectives {
                             if (countNew == 0)
                                 return completeWithFuture(singleReq(url));
                             else
-                                return completeWithFuture(Patterns.ask(storeActor ,new GetServerMsg(url) ))
+                                return completeWithFuture(Patterns.ask(storeActor ,new GetServerMsg("Req"), ))
                         })
 
                 )
