b69b25122a096590a704d54fd16607591a2ea3fa
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-22 03:50:39 +0300

    Scripted auto-commit on change (2020-12-22 03:50:39) by gitwatch.sh

--- src/main/java/bmstu/MainZooApplication.java
+++ src/main/java/bmstu/MainZooApplication.java
@@ -14,24 +14,30 @@ import akka.stream.ActorMaterializer;
 import akka.stream.Materializer;
 import akka.stream.javadsl.Flow;
 
+import java.util.concurrent.CompletionStage;
+
 public class MainZooApplication  extends AllDirectives {
 //    моя лаба 4 оттуда код
-    final ActorSystem system = ActorSystem.create("test");
-    ActorRef router = system.actorOf(Props.create(ActorRouter.class));
-    final Http http = Http.get(system);
-    final AkkaMainApplication app = new AkkaMainApplication();
-    final Materializer materializer = ActorMaterializer.create(system);
-    final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
-            app.createRoute(system , router).flow(system, materializer);
-    final CompletionStage<ServerBinding> binding = http.bindAndHandle(
-            routeFlow,
-            ConnectHttp.toHost("localhost", PORT),
-            materializer
-    );
+public static final int TIMEOUT_MILLIS = 5000;
+    public static final int PORT = 8080;
+
+    public static void main(String[] args) throws IOException {
+        final ActorSystem system = ActorSystem.create("test");
+        ActorRef router = system.actorOf(Props.create(ActorRouter.class));
+        final Http http = Http.get(system);
+        
+        final Materializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                app.createRoute(system, router).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", PORT),
+                materializer
+        );
         System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
         System.in.read();
         binding
                 .thenCompose(ServerBinding::unbind)
-            .thenAccept(unbound -> system.terminate());
-
+                .thenAccept(unbound -> system.terminate());
+    }
 }
