631ec4685863479c7ff533273cb3bc083b13dc3d
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-22 22:49:38 +0300

    Scripted auto-commit on change (2020-12-22 22:49:38) by gitwatch.sh

--- src/main/java/bmstu/ServerChangerdMsg.java
+++ src/main/java/bmstu/ServerChangerdMsg.java
@@ -1,5 +1,17 @@
 package bmstu;
 
 public class ServerChangerdMsg {
-    private String[] 
+    private String[] servers;
+
+    public ServerChangerdMsg(String[] servers) {
+        this.servers = servers;
+    }
+
+    public String[] getServers() {
+        return servers;
+    }
+
+    public void setServers(String[] servers) {
+        this.servers = servers;
+    }
 }
