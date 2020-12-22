04716bc3f2d62d9191ab8ab72f1c11e2d963f366
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-23 00:41:11 +0300

    Scripted auto-commit on change (2020-12-23 00:41:11) by gitwatch.sh

--- src/main/java/bmstu/Zoo.java
+++ src/main/java/bmstu/Zoo.java
@@ -16,7 +16,7 @@ public class Zoo {
         this.storeActor = storeActor;
         this.zoo = new ZooKeeper("localhost", 3000, null);
         serverInit();
-        WatcherEvent e = new WatcherEvent()
+        WatcherEvent e = new WatcherEvent(Watcher.Event.EventType.NodeCreated)
     }
 
     private static void serverInit() throws KeeperException, InterruptedException {
