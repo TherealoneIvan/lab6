8972273d07bb312eb132f7151e769cf219bf7fb6
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-22 03:23:25 +0300

    Scripted auto-commit on change (2020-12-22 03:23:25) by gitwatch.sh

--- src/main/java/bmstu/StoreConfActor.java
+++ src/main/java/bmstu/StoreConfActor.java
@@ -3,7 +3,7 @@ package bmstu;
 import akka.actor.AbstractActor;
 
 public class StoreConfActor extends AbstractActor {
-    
+    private 
     @Override
     public Receive createReceive() {
         return null;
