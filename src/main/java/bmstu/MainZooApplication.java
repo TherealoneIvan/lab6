4afc4047953688326d7ba74ab116719ae932a808
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-23 00:22:32 +0300

    Scripted auto-commit on change (2020-12-23 00:22:31) by gitwatch.sh

--- src/main/java/bmstu/MainZooApplication.java
+++ src/main/java/bmstu/MainZooApplication.java
@@ -27,6 +27,7 @@ public class MainZooApplication  extends AllDirectives {
         ActorSystem system = ActorSystem.create("routes");
         ActorRef storeActor = system.actorOf(Props.create(StoreConfActor.class));
         final Http http = Http.get(system);
+
         final MainZooApplication app = new MainZooApplication();
         final ActorMaterializer materializer =
                 ActorMaterializer.create(system);
