214ca56732825678cba963664f7ef484fc4d7278
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-22 20:03:21 +0300

    Scripted auto-commit on change (2020-12-22 20:03:21) by gitwatch.sh

--- src/main/java/bmstu/Zoo.java
+++ src/main/java/bmstu/Zoo.java
@@ -4,10 +4,12 @@ import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 
+import java.util.List;
+
 public class Zoo {
     ZooKeeper zoo = new ZooKeeper("1MB27.0.0.1MB:21MB81MB", 3000, this);
     zoo.create("/servers/s",
-        “data”.getBytes(),
+        data.getBytes(),
     ZooDefs.Ids.OPEN_ACL_UNSAFE ,
     CreateMode.EPHEMERAL_SEQUENTIAL
 );
