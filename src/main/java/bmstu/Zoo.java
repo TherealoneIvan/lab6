10245e8d54d6e5132eac5009ef047a9a36a180f2
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-22 21:50:24 +0300

    Scripted auto-commit on change (2020-12-22 21:50:24) by gitwatch.sh

--- src/main/java/bmstu/Zoo.java
+++ src/main/java/bmstu/Zoo.java
@@ -9,7 +9,7 @@ public class Zoo {
     String a = "asd";
     ZooKeeper zoo = new ZooKeeper("localhost", 3000, null);
     zoo.create("/servers/s",
-            a.getBytes(),
+            ("loclahost").getBytes(),
     ZooDefs.Ids.OPEN_ACL_UNSAFE ,
     CreateMode.EPHEMERAL_SEQUENTIAL);
 
