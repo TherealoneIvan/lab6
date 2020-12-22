93380a709a3b782cded7650e3a3041b934345e60
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-22 03:25:49 +0300

    Scripted auto-commit on change (2020-12-22 03:25:49) by gitwatch.sh

--- src/main/java/bmstu/GetServerMsg.java
+++ src/main/java/bmstu/GetServerMsg.java
@@ -1,2 +1,17 @@
-package bmstu;public class GetServerMsg {
+package bmstu;
+
+public class GetServerMsg {
+    private String getMsg;
+
+    public GetServerMsg(String getMsg) {
+        this.getMsg = getMsg;
+    }
+
+    public String getGetMsg() {
+        return getMsg;
+    }
+
+    public void setGetMsg(String getMsg) {
+        this.getMsg = getMsg;
+    }
 }
